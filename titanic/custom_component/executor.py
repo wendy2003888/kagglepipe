cf78c66939d386337b8fbc6417207daec49c3001
Wendy Huang <wendycbh@google.com>
2020-05-24 22:02:37 -0700

    a workable version, not sort yet

--- titanic/custom_component/executor.py
+++ titanic/custom_component/executor.py
@@ -1,8 +1,4 @@
 # Lint as: python3
-"""TODO(wendycbh): DO NOT SUBMIT without one-line documentation for component.
-
-TODO(wendycbh): DO NOT SUBMIT without a detailed description of component.
-"""
 
 import os
 from typing import Any, Dict, List, Text
@@ -14,6 +10,24 @@ from tfx.components.base import base_executor
 from tfx.types import artifact_utils
 from tfx.utils import io_utils
 from tfx.utils import path_utils
+
+class RunModel(beam.DoFn):
+  def __init__(self, model_path, signature_name, id_label):
+    self.model_path = model_path
+    self.signature_name = signature_name
+    self.tags = [tf.saved_model.SERVING] # == 'serve' => tensorflow/python/saved_model/tag_constants.py
+    self.session = None
+    self.model = None
+    self.id_label = id_label
+    
+  def setup(self):
+    self.model = tf.keras.models.load_model(self.model_path) 
+    
+  def process(self, elements):
+
+    examples = tf.constant([elements.SerializeToString()])
+    outputs = self.model.signatures[self.signature_name](examples=examples)
+    return [{'id': elements.features.feature[self.id_label], 'pred': outputs}]
 
 # Not running
 class Executor(base_executor.BaseExecutor):
@@ -33,9 +47,6 @@ class Executor(base_executor.BaseExecutor):
     model_path = path_utils.serving_model_path(model.uri)
     absl.logging.info('Using {} as current model.'.format(model_path))
     
-    live_model = tf.keras.models.load_model(model_path)
-    absl.logging.info('Model info: {}'.format(live_model.summary())) 
-    
     output_uri = os.path.join(
         artifact_utils.get_single_uri(output_dict['output_data']), 'pred.csv')
     with self._make_beam_pipeline() as pipeline:
@@ -45,9 +56,24 @@ class Executor(base_executor.BaseExecutor):
       
       (test_data | 'Flattern' >> beam.Flatten()
       | 'ParseToExample' >> beam.Map(tf.train.Example.FromString)
-      # can't pickle tensorflow.python._tf_stack.StackSummary objects
-      | 'Predict' >> beam.Map(lambda x: {'id': x.features.feature['PassengerId'].int64_list.value[0], 'prediction': live_model.signatures["serving_default"](examples=tf.constant([x.SerializeToString()]))})
-      | 'PredictionTensorToNumpy' >> beam.Map(lambda x: x['output_0'].numpy())
-      | 'FormatToStr' >> beam.Map(lambda x: '%s,%s' % (x[0], '1' if x[1][0][0] > 0.5  else '0'))
-      | 'WriteToFile' >> beam.io.WriteToText(output_uri))
+      | 'Prediction' >> beam.ParDo(RunModel(model_path, 'serving_default', 'PassengerId'))
+      | 'ParseToKVPair' >> beam.Map(lambda x: ParseResultToKV(x))
+      | 'ToStr' >> beam.Map(lambda x: '{},{}'.format(x[0], '0' if x[1] < 0.5 else '1'))
+      | 'WriteToFile' >> beam.io.WriteToText(output_uri, num_shards=1, shard_name_template='', header='PassengerId,Survived'))
     absl.logging.info('TestPredComponent result written to %s.', output_uri)
+
+
+def ParseResultToKV(outputs):
+  # res = {}
+  # if len(outputs['id'].int64_list.value):
+  #   res['id'] = list(outputs['id'].int64_list.value)
+  # # 'output_0' is the outputs key in SignatureDef
+  # res['pred'] = outputs['pred']['output_0'].numpy()
+  id_val = None
+  if len(outputs['id'].int64_list.value):
+    id_val = outputs['id'].int64_list.value[0]
+  pred = outputs['pred']['output_0'].numpy().ravel()
+  label = 0 if pred[0] < 0.5 else 1
+  return (id_val, label)
+
+
